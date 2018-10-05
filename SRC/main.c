c5f928ae0e664a8a947837affb3ad30a93841cad
Sptounissou <reynald.barbeaut@gmail.com>
2018-10-05 09:20:19 +0200

    Début du menu

--- SRC/main.c
+++ SRC/main.c
@@ -19,34 +19,9 @@
 /*---------------------*/
 
 int main() {
-	struct Neighbour zero, first, second, third, empty;
-	neighbour_create(&first,1,1);
-	neighbour_create(&second,2,2);
-	neighbour_create(&third,3,3);
-	neighbour_create(&zero,-1,0);
-	neighbour_create(&empty,0,0);
-	printf("Beginning of the programm :\n");
 
-	printf("Front: ");
-	neighbour_add_front(&zero,&third);
-	neighbour_add_front(&third,&second);
-	neighbour_add_front(&second,&first);
-
-	list_dump(&first);
-	printf("\n");
-
-	neighbour_destroy(&first);
-
-	list_dump(&second);
-	printf("\n");
-
-
-	neighbour_list_destroy(&second);
-	printf("Destroy :");
-	list_dump(&second);
-	printf("\n");
-
-	/*
+	char *string = malloc(sizeof(char *));
+	int res = 0, cmp = 1;
 	printf("Possibles commands for this programm : \n");
 	printf("\t create graph : graph creation\n");
 	printf("\t load graph : graph loading from a text file\n");
@@ -56,8 +31,23 @@ int main() {
 	printf("\t remove edge : deletion of an edge from the graph\n");
 	printf("\t view graph : graph display\n");
    	printf("\t save graph : graph saving in the text format\n");
+	printf("\t help : print this text \n");
 	printf("\t quit : exit the program\n");
-	*/
+
+	
+	while(cmp != 0){
+		res = scanf("%s",string);
+		if(res == 0)
+		{
+			printf("The character given is not a string !\n");
+			exit(0);
+		}
+		cmp = strcmp(string,"quit");
+		
+
+	}
+	
+
 
 
 }
