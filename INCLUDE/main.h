1a17e20c77d2111ccfde0f9bd2163e29362cf040
Sptounissou <reynald.barbeaut@gmail.com>
2018-10-05 20:59:18 +0200

    Création du menu

--- INCLUDE/main.h
+++ INCLUDE/main.h
@@ -2,3 +2,10 @@
 #include <stdlib.h>
 
 #include "list.h"
+
+
+void clean_stdin(void){
+	int c = 0;
+	while ((c = getchar()) != '\n' && c != EOF);
+
+}
--- SRC/main.c
+++ SRC/main.c
@@ -14,6 +14,9 @@
 
 #include "main.h"
 
+
+
+
 /*---------------------*/
 /* Main programm */
 /*---------------------*/
@@ -22,32 +25,61 @@ int main() {
 
 	char *string = malloc(sizeof(char *));
 	int res = 0, cmp = 1;
-	printf("Possibles commands for this programm : \n");
-	printf("\t create graph : graph creation\n");
-	printf("\t load graph : graph loading from a text file\n");
-	printf("\t add neighbour : insertion of a neighbour in the graph\n");
-	printf("\t add edge : insertion of an edge in the graph\n");
-	printf("\t remove neighbour : deletion of a neighbour from the graph\n");
-	printf("\t remove edge : deletion of an edge from the graph\n");
-	printf("\t view graph : graph display\n");
-   	printf("\t save graph : graph saving in the text format\n");
-	printf("\t help : print this text \n");
-	printf("\t quit : exit the program\n");
+	char help[512];
+	strcpy(help,"Possibles commands for this programm : \n");
+	strcat(help, "\t create graph : graph creation\n");
+	strcat(help,"\t load graph : graph loading from a text file\n");
+	strcat(help,"\t add neighbour : insertion of a neighbour in the graph\n");
+	strcat(help,"\t add edge : insertion of an edge in the graph\n");
+	strcat(help,"\t remove neighbour : deletion of a neighbour from the graph\n");
+	strcat(help,"\t remove edge : deletion of an edge from the graph\n");
+	strcat(help,"\t view graph : graph display\n");
+	strcat(help,"\t save graph : graph saving in the text format\n");
+	strcat(help,"\t help : display know commands\n");
+	strcat(help,"\t quit : exit the program\n");
 
+
+	printf("%s",help);
 	
 	while(cmp != 0){
-		res = scanf("%s",string);
+		res = scanf("%[^\n]",string);
+		clean_stdin();
 		if(res == 0)
 		{
 			printf("The character given is not a string !\n");
-			exit(0);
-		}
-		cmp = strcmp(string,"quit");
-		
+		}else{
+			cmp = strcmp(string,"quit");
+
+			if(strcmp(string,"create graph") == 0){
+				//Create graph
+				printf("Graph created.\n");
+			}else if(strcmp(string,"load graph") == 0){
+				//Load graph
+				printf("Graph loaded.\n");
+			}else if(strcmp(string,"add neighbour") == 0){
+				//Add neighbour
+				printf("Neighbour added.\n");
+			}else if(strcmp(string,"add edge") == 0){
+				//Add edge
+				printf("Edge added.\n");
+			}else if(strcmp(string,"remove neighbour") == 0){
+				//Remove neighbour
+				printf("Neighbour removed.\n");
+			}else if(strcmp(string,"remove edge") == 0){
+				//Remove edge
+				printf("Edge removed.\n");
+			}else if(strcmp(string,"view graph") == 0){
+				//Print graph to stdout
+			}else if(strcmp(string,"save graph") == 0){
+				//Save graph
+				printf("Graph saved.\n");
+			}else if(strcmp(string,"help") == 0){
+				printf("%s",help);
+			}else{
+				printf("Error unknown command !\n");
+				printf("%s",help);		
+			}	
+		}	
 
 	}
-	
-
-
-
 }
